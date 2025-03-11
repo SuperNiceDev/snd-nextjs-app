5bb8d36ac7f5f64fd71554d08aed1e0c1f79df8c
Philipp Neumann <p.neumann@supernice-dev.com>
2025-03-11 19:09:14 +0100

    improve next auth linkedin provider

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 import NextAuth, { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import LinkedinProvider from "next-auth/providers/linkedin";
+import LinkedinProvider, {
+  LinkedInProfile,
+} from "next-auth/providers/linkedin";
 
 const STRAPI_DOMAIN = process.env.NEXT_PUBLIC_CMS_DOMAIN;
 
@@ -12,6 +14,8 @@ export const authOptions: NextAuthOptions = {
   secret: process.env.NEXTAUTH_SECRET as string,
   providers: [
     GoogleProvider({
+      // https://next-auth.js.org/providers/google
+      // https://console.cloud.google.com/auth/clients/551670901458-q7q1j4c7f1rs187o7hi8gcsei64be523.apps.googleusercontent.com
       clientId: process.env.NEXTAUTH_GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.NEXTAUTH_GOOGLE_CLIENT_SECRET as string,
       authorization: {
@@ -23,10 +27,24 @@ export const authOptions: NextAuthOptions = {
       },
     }),
     LinkedinProvider({
+      // https://next-auth.js.org/providers/linkedin
       // https://www.linkedin.com/developers/apps/219502882/auth
-      // http://localhost:3000/api/auth/callback/linkedin
       clientId: process.env.NEXTAUTH_LINKEDIN_CLIENT_ID as string,
       clientSecret: process.env.NEXTAUTH_LINKEDIN_CLIENT_SECRET as string,
+      issuer: "https://www.linkedin.com",
+      profile: (profile: LinkedInProfile) => ({
+        id: profile.sub,
+        name: profile.name,
+        email: profile.email,
+        image: profile.picture,
+      }),
+      wellKnown:
+        "https://www.linkedin.com/oauth/.well-known/openid-configuration",
+      authorization: {
+        params: {
+          scope: "openid profile email",
+        },
+      },
     }),
   ],
   session: {
@@ -37,56 +55,67 @@ export const authOptions: NextAuthOptions = {
   },
   callbacks: {
     async signIn({ account, profile }: any) {
-      // console.log("--------------------------------");
+      // console.log("-------------------------------");
       // console.log("signIn() account: ", account);
-      // console.log("--------------------------------");
+      // console.log("-------------------------------");
       if (account.provider === "google") {
         return profile.email_verified;
       }
       return true;
     },
 
-    async jwt({ token, account }) {
-      if (account) {
-        // console.log("--------------------------------");
-        // console.log("jwt() account: ", account);
-        // console.log("- - - - - - - - - - - - - - - -");
+    async jwt({ account, token }) {
+      console.log("-------------------------------");
+      console.log("jwt() account: ", account);
+      console.log("- - - - - - - - - - - - - - - -");
+      console.log("jwt() token: ", token);
+      console.log("- - - - - - - - - - - - - - - -");
 
-        const strapiRes = await axios.get(
-          `${STRAPI_DOMAIN}/api/auth/${account?.provider}/callback?access_token=${account?.access_token}`,
+      if (account) {
+        const { provider, access_token } = account;
+
+        const strapiAuthRes = await axios.get(
+          `${STRAPI_DOMAIN}/api/auth/${provider}/callback?access_token=${access_token}`,
         );
 
-        const jwt = strapiRes.data.jwt;
-        // console.log("jwt() jwt: ", jwt);
-        // console.log("- - - - - - - - - - - - - - - -");
+        console.log(
+          "jwt() strapi /api/auth/ strapiAuthRes.data: ",
+          strapiAuthRes.data,
+        );
+        const strapiJwt = strapiAuthRes.data?.jwt;
+        console.log("jwt() strapi /api/auth/ strapiJwt: ", strapiJwt);
+        console.log("- - - - - - - - - - - - - - - -");
 
-        const res = await axios({
-          method: "PUT",
-          url: `${STRAPI_DOMAIN}/api/users/me`,
-          data: JSON.stringify({
-            lastLogin: new Date().getTime(),
-          }),
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${jwt}`,
-          },
-        });
+        if (strapiJwt) {
+          // token.jwt = strapiJwt;
 
-        // console.log("session() res: ", res?.data);
-        // console.log("--------------------------------");
+          const strapiUsersMeRes = await axios({
+            method: "PUT",
+            url: `${STRAPI_DOMAIN}/api/users/me`,
+            data: JSON.stringify({
+              lastLogin: new Date().getTime(),
+            }),
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${strapiJwt}`,
+            },
+          });
+          console.log("jwt() strapi /api/users/me : ", strapiUsersMeRes?.data);
+          console.log("-------------------------------");
+        }
       }
 
       return token;
     },
 
     async session({ session, token, user }) {
-      // console.log("--------------------------------");
+      // console.log("-------------------------------");
       // console.log("session() session: ", session);
       // console.log("- - - - - - - - - - - - - - - -");
       // console.log("session() token: ", token);
       // console.log("- - - - - - - - - - - - - - - -");
       // console.log("session() user: ", user);
-      // console.log("--------------------------------");
+      // console.log("-------------------------------");
 
       // session.user = token;
       return session;
