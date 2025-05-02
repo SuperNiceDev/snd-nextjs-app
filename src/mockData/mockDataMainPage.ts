ad7b8785cbf9f14a2fed7fce32fa1f248e70a2c0
Philipp Neumann <p.neumann@supernice-dev.com>
2025-05-02 11:53:41 +0200

    update mock data

---
+++ src/mockData/mockDataMainPage.ts
@@ -0,0 +1,37 @@
+export const mockDataMainPage = {
+  data: [
+    {
+      id: 1,
+      title: "Home",
+      slug: "/",
+      createdAt: "2024-05-02T14:44:40.027Z",
+      updatedAt: "2025-04-26T08:37:28.688Z",
+      publishedAt: "2024-05-03T07:43:14.210Z",
+      locale: "en",
+      documentId: "go03i9e4vusvq3vv0673nwft",
+      sections: [
+        {
+          __component: "blocks.row",
+          id: 12,
+          rowId: "rowId0",
+          richText: [
+            {
+              id: 2,
+              text: "Home - sections - row - richText - text",
+              size: "xs",
+            },
+          ],
+          link: [],
+        },
+      ],
+    },
+  ],
+  meta: {
+    pagination: {
+      page: 1,
+      pageSize: 25,
+      pageCount: 1,
+      total: 1,
+    },
+  },
+};
