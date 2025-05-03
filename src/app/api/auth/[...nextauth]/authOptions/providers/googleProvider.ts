3ca7156928c1db8eed3dc2683a82b652043c7a71
Philipp Neumann <p.neumann@supernice-dev.com>
2025-05-03 11:19:24 +0200

    update auth providers

--- src/app/api/auth/[...nextauth]/authOptions/providers/googleProvider.ts
+++ src/app/api/auth/[...nextauth]/authOptions/providers/googleProvider.ts
@@ -1,15 +1,6 @@
 import GoogleProvider from "next-auth/providers/google";
 
 export const googleProvider = GoogleProvider({
-  // https://next-auth.js.org/providers/google
-  // https://console.cloud.google.com/auth/clients/551670901458-q7q1j4c7f1rs187o7hi8gcsei64be523.apps.googleusercontent.com
   clientId: process.env.NEXTAUTH_GOOGLE_CLIENT_ID as string,
   clientSecret: process.env.NEXTAUTH_GOOGLE_CLIENT_SECRET as string,
-  // authorization: {
-  //   params: {
-  //     prompt: "consent",
-  //     access_type: "offline",
-  //     response_type: "code",
-  //   },
-  // },
 });
--- src/app/api/auth/[...nextauth]/authOptions/providers/linkedinProvider.ts
+++ src/app/api/auth/[...nextauth]/authOptions/providers/linkedinProvider.ts
@@ -3,11 +3,8 @@ import LinkedinProvider, {
 } from "next-auth/providers/linkedin";
 
 export const linkedinProvider = LinkedinProvider({
-  // https://next-auth.js.org/providers/linkedin
-  // https://www.linkedin.com/developers/apps/219502882/auth
   clientId: process.env.NEXTAUTH_LINKEDIN_CLIENT_ID as string,
   clientSecret: process.env.NEXTAUTH_LINKEDIN_CLIENT_SECRET as string,
-  // issuer: "https://www.linkedin.com",
   profile: (profile: LinkedInProfile) => ({
     id: profile.sub,
     name: profile.name,
