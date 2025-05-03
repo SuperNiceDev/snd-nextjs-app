3c6d8ddae88abb9b3a89c9c91e2974f39f20b301
Philipp Neumann <p.neumann@supernice-dev.com>
2025-05-03 11:19:46 +0200

    remove unused middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,9 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-// import cmsRedirectsMw from "./middleware/cmsRedirectsMw";
 import urlParamsMw from "./middleware/urlParamsMw";
 
 const middleware = async (req: NextRequest) => {
-  // await cmsRedirectsMw(req);
   const headers = urlParamsMw(req);
 
   return NextResponse.next({
