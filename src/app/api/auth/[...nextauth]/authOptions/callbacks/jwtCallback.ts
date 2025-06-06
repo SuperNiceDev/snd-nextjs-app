db7f245586f0a74081975763d940b37d9cf44339
Philipp Neumann <p.neumann@supernice-dev.com>
2025-06-06 11:11:44 +0200

    feat(feat/add-shadcn): comment out strapi auth provider logic

--- src/app/api/auth/[...nextauth]/authOptions/callbacks/jwtCallback.ts
+++ src/app/api/auth/[...nextauth]/authOptions/callbacks/jwtCallback.ts
@@ -2,49 +2,53 @@ import axios from "axios";
 
 const DOMAIN = process.env.NEXT_PUBLIC_CMS_DOMAIN;
 
-export async function jwtCallback({ account, token }: any) {
+export async function jwtCallback(args: any) {
+  const { account, token } = args;
+
   console.log("-------------------------------");
   console.log("------------------------------- jwt() - start");
   console.log("-------------------------------");
+  console.log("jwt() args: ", args);
+  console.log("- - - - - - - - - - - - - - - -");
   console.log("jwt() account: ", account);
   console.log("- - - - - - - - - - - - - - - -");
   console.log("jwt() token: ", token);
   console.log("- - - - - - - - - - - - - - - -");
 
-  if (account) {
-    const { provider, access_token } = account;
+  // if (account) {
+  //   const { provider, access_token } = account;
 
-    const strapiAuthRes = await axios.get(
-      `${DOMAIN}/api/auth/${provider}/callback?access_token=${access_token}`,
-    );
+  //   const strapiAuthRes = await axios.get(
+  //     `${DOMAIN}/api/auth/${provider}/callback?access_token=${access_token}`,
+  //   );
 
-    console.log(
-      "jwt() strapi /api/auth/ strapiAuthRes.data: ",
-      strapiAuthRes.data,
-    );
-    console.log("- - - - - - - - - - - - - - - -");
-    const strapiJwt = strapiAuthRes.data?.jwt;
-    console.log("jwt() strapi /api/auth/ strapiJwt: ", strapiJwt);
-    console.log("- - - - - - - - - - - - - - - -");
+  //   console.log(
+  //     "jwt() strapi /api/auth/ strapiAuthRes.data: ",
+  //     strapiAuthRes.data,
+  //   );
+  //   console.log("- - - - - - - - - - - - - - - -");
+  //   const strapiJwt = strapiAuthRes.data?.jwt;
+  //   console.log("jwt() strapi /api/auth/ strapiJwt: ", strapiJwt);
+  //   console.log("- - - - - - - - - - - - - - - -");
 
-    if (strapiJwt) {
-      // token.jwt = strapiJwt;
+  //   if (strapiJwt) {
+  //     // token.jwt = strapiJwt;
 
-      const strapiUsersMeRes = await axios({
-        method: "PUT",
-        url: `${DOMAIN}/api/users/me`,
-        data: JSON.stringify({
-          lastLogin: new Date().getTime(),
-        }),
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${strapiJwt}`,
-        },
-      });
-      console.log("jwt() strapi /api/users/me : ", strapiUsersMeRes?.data);
-      console.log("- - - - - - - - - - - - - - - -");
-    }
-  }
+  //     const strapiUsersMeRes = await axios({
+  //       method: "PUT",
+  //       url: `${DOMAIN}/api/users/me`,
+  //       data: JSON.stringify({
+  //         lastLogin: new Date().getTime(),
+  //       }),
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //         Authorization: `Bearer ${strapiJwt}`,
+  //       },
+  //     });
+  //     console.log("jwt() strapi /api/users/me : ", strapiUsersMeRes?.data);
+  //     console.log("- - - - - - - - - - - - - - - -");
+  //   }
+  // }
 
   console.log("-------------------------------");
   console.log("------------------------------- jwt() - end");
