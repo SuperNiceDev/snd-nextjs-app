b6549b4c4af113cb2ac26287e50b8effe923045d
Philipp Neumann <p.neumann@supernice-dev.com>
2025-07-23 11:45:06 +0200

    feat(feat/add-shadcn): improve MainPage

--- src/app/[[...slug]]/MainPage.tsx
+++ src/app/[[...slug]]/MainPage.tsx
@@ -3,43 +3,37 @@ import { notFound } from "next/navigation";
 
 import Main from "@/components/Main/Main";
 import { mockDataMainPage } from "@/mockData/mockDataMainPage";
-import { PageParamsType } from "@/types/types";
 import axiosInstance from "@/utils/axiosInstance";
 import getNavPageApiUrl from "@/utils/getNavPageApiUrl";
 
 type MainPagePropsType = {
-  params: PageParamsType;
-};
-
-type ApiResType = {
-  data: {
-    sections: object[];
-  }[];
+  params: Promise<{ slug: string[] }>;
 };
 
 export default async function MainPage({ params }: MainPagePropsType) {
-  const { slug } = await params;
-  const path = `/${slug?.join("/") || ""}`;
-  const url = getNavPageApiUrl(slug);
+  const { slug: pSlug } = await params;
+  const slug = `/${pSlug?.join("/") || ""}`;
+  const url = getNavPageApiUrl(pSlug);
 
   let resData;
   try {
-    const res: AxiosResponse<ApiResType> = await axiosInstance.get(url);
+    const res: AxiosResponse = await axiosInstance.get(url);
     resData = res.data;
   } catch (err) {
     console.warn("MainPage() err: ", err);
     resData = mockDataMainPage;
   }
 
-  const data = resData?.data;
+  const resData2 = resData?.data;
 
-  if ((path && !data?.length) || (!path && !data)) {
+  if ((slug && !resData2?.length) || (!slug && !resData2)) {
     notFound();
   }
 
-  const dataTmp = Array.isArray(data) ? data[0] : data;
-  const sections = dataTmp?.sections;
-  // console.log("MainPage() dataTmp: ", dataTmp);
+  const attributes = resData2?.length ? resData2?.[0] : resData2;
+  const sections = attributes?.sections;
+
+  // console.log("MainPage() attributes:   ", attributes);
 
   return <Main sections={sections} />;
 }
