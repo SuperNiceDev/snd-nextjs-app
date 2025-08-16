ec2df3af81e0e0f68f4270b8eaa1017deb51a07e
Philipp Neumann <p.neumann@supernice-dev.com>
2025-08-16 11:01:02 +0200

    feat(feat/add-shadcn): improve auth callback

--- src/app/api/auth/[...nextauth]/authOptions/callbacks/jwtCallback.ts
+++ src/app/api/auth/[...nextauth]/authOptions/callbacks/jwtCallback.ts
@@ -6,52 +6,66 @@ export async function jwtCallback(args: any) {
   const { account, token } = args;
 
   console.log("-------------------------------");
-  console.log("------------------------------- jwt() - start");
+  console.warn("------------------------------- jwtCallback() - start");
   console.log("-------------------------------");
-  console.log("jwt() args: ", args);
+  console.log("jwtCallback() args: ", args);
   console.log("- - - - - - - - - - - - - - - -");
-  console.log("jwt() account: ", account);
+  console.log("jwtCallback() account: ", account);
   console.log("- - - - - - - - - - - - - - - -");
-  console.log("jwt() token: ", token);
+  console.log("jwtCallback() token: ", token);
   console.log("- - - - - - - - - - - - - - - -");
 
-  // if (account) {
-  //   const { provider, access_token } = account;
+  try {
+    if (account) {
+      const { provider, access_token } = account;
 
-  //   const strapiAuthRes = await axios.get(
-  //     `${DOMAIN}/api/auth/${provider}/callback?access_token=${access_token}`,
-  //   );
+      token.accessToken = access_token;
 
-  //   console.log(
-  //     "jwt() strapi /api/auth/ strapiAuthRes.data: ",
-  //     strapiAuthRes.data,
-  //   );
-  //   console.log("- - - - - - - - - - - - - - - -");
-  //   const strapiJwt = strapiAuthRes.data?.jwt;
-  //   console.log("jwt() strapi /api/auth/ strapiJwt: ", strapiJwt);
-  //   console.log("- - - - - - - - - - - - - - - -");
+      const strapiAuthUrl = `${DOMAIN}/api/auth/${provider}/callback?access_token=${access_token}`;
+      console.log("jwtCallback() strapiAuthUrl: ", strapiAuthUrl);
 
-  //   if (strapiJwt) {
-  //     // token.jwt = strapiJwt;
+      const strapiAuthRes = await axios(strapiAuthUrl).catch((err) => {
+        console.error("jwtCallback() /api/auth/ error: ", err);
+      });
 
-  //     const strapiUsersMeRes = await axios({
-  //       method: "PUT",
-  //       url: `${DOMAIN}/api/users/me`,
-  //       data: JSON.stringify({
-  //         lastLogin: new Date().getTime(),
-  //       }),
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //         Authorization: `Bearer ${strapiJwt}`,
-  //       },
-  //     });
-  //     console.log("jwt() strapi /api/users/me : ", strapiUsersMeRes?.data);
-  //     console.log("- - - - - - - - - - - - - - - -");
-  //   }
-  // }
+      const strapiAuthData = strapiAuthRes?.data;
+      console.log("jwtCallback() strapiAuthData.jwt: ", strapiAuthData?.jwt);
+      console.log("jwtCallback() trapiAuthData.user: ", strapiAuthData?.user);
+      console.log("- - - - - - - - - - - - - - - -");
+
+      if (strapiAuthData) {
+        // token.jwt = strapiAuthData?.jwt;
+        token.strapiJwt = strapiAuthData?.jwt;
+        token.strapiUser = strapiAuthData?.user;
+
+        const strapiUsersMeRes = await axios({
+          method: "PUT",
+          url: `${DOMAIN}/api/users/me`,
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${strapiAuthData.jwt}`,
+          },
+          data: JSON.stringify({
+            lastLogin: new Date().getTime(),
+          }),
+        }).catch((err) => {
+          console.error("jwtCallback() /api/users/me error: ", err);
+        });
+
+        console.log(
+          "jwtCallback() strapiUsersMeRes?.data: ",
+          strapiUsersMeRes?.data,
+        );
+        console.log("- - - - - - - - - - - - - - - -");
+      }
+    }
+  } catch (err) {
+    console.error("jwtCallback() error: ", err);
+  }
 
   console.log("-------------------------------");
-  console.log("------------------------------- jwt() - end");
+  console.warn("------------------------------- jwtCallback() - end");
   console.log("-------------------------------");
+
   return token;
 }
--- src/app/api/auth/[...nextauth]/authOptions/callbacks/sessionCallback.ts
+++ src/app/api/auth/[...nextauth]/authOptions/callbacks/sessionCallback.ts
@@ -5,8 +5,17 @@ export async function sessionCallback({ session, token, user }: any) {
   // console.log("session() token: ", token);
   // console.log("- - - - - - - - - - - - - - - -");
   // console.log("session() user: ", user);
-  // console.log("-------------------------------");
 
   // session.user = token;
+
+  session.accessToken = token.accessToken;
+  session.strapiJwt = token.strapiJwt;
+  session.strapiUser = token.strapiUser;
+
+  // console.log("- - - - - - - - - - - - - - - -");
+  // console.log("session() session: ", session);
+  // console.log("session() session.accessToken: ", session.accessToken);
+  // console.log("-------------------------------");
+
   return session;
 }
